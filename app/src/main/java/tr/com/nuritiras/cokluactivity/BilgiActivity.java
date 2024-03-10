fe49d94a163ab2ff47ae6b361466307110db5c1b
Nuri TIRAŞ <47992544+nuritiras@users.noreply.github.com>
2024-03-10 10:59:36 +0300

    Uygulama son

--- app/src/main/java/tr/com/nuritiras/cokluactivity/BilgiActivity.java
+++ app/src/main/java/tr/com/nuritiras/cokluactivity/BilgiActivity.java
@@ -2,26 +2,20 @@ package tr.com.nuritiras.cokluactivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-import tr.com.nuritiras.cokluactivity.databinding.ActivityMainBinding;
+import tr.com.nuritiras.cokluactivity.databinding.ActivityBilgiBinding;
 
 public class BilgiActivity extends AppCompatActivity {
-    private ActivityMainBinding binding;
+    private ActivityBilgiBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        binding= ActivityBilgiBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         Intent intent=getIntent();
         String bilgiAdiSoyAdi= intent.getStringExtra("adiSoyadiKey");
         String bilgiTelefonNo=intent.getStringExtra("telefonNoKey");
-        binding.editTextAdiSoyad.setText(bilgiAdiSoyAdi);
-        binding.editTextTelefon.setText(bilgiTelefonNo);
+        binding.textViewAdiSoyadi.setText(bilgiAdiSoyAdi);
+        binding.textViewTelefonNo.setText(bilgiTelefonNo);
     }
 }
